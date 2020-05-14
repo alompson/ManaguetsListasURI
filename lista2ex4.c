cd877e76386c1861aa4f4c61eaf4cca247895098
Joao Alonso <alonso963@hotmail.com>
2020-05-14 09:54:48 -0300

    mais um ex

--- lista2ex4.c
+++ lista2ex4.c
@@ -1,14 +1,34 @@
 #include <stdio.h>
 
 void main(void){
-    int ordem, lin, col;
+    int ordem, lin, col, i;
     scanf("%d", &ordem);
-    int mat[ordem][ordem];
-
+    while(ordem != 0){
+        int mat[ordem][ordem];
         for (lin = 0; lin < ordem; lin++)
         {
+            printf ("  ");
             for (col = 0; col < ordem; col++)
             {
-
+                for (i = 0; i < ordem/2; i++)
+                {
+                    if ((lin==i || lin==ordem-1) && (col==i || col==ordem-1))
+                    {
+                        mat[lin][col] = i+1;
+                        printf ("%d  ", mat[lin][col]);
+                        break;
+                    }
+                }
             }
+        printf("\n");
         }
+        printf ("\n\n");
+        scanf("%d", &ordem);
+    }
+}
+
+
+
+
+
+
